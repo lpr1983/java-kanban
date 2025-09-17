127c22e06539d03624eff4e9cf23b0d1eb7cac84
Leonid Proskuryakov <lpr@inbox.ru>
2025-09-17 10:26:38 +0300

    По замечаниям ревьюера: 1. Замена константы static на обычное поле. 2. Замена объявления поля ArrayList<> на List<>.

--- src/practicum/taskmanager/InMemoryHistoryManager.java
+++ src/practicum/taskmanager/InMemoryHistoryManager.java
@@ -1,14 +1,15 @@
 package practicum.taskmanager;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager{
-    static final int MAX_HISTORY_SIZE = 10;
-    private final ArrayList<Task> historyList = new ArrayList<>();
+    final int maxHistorySize = 10;
+    private final List<Task> historyList = new ArrayList<>();
 
     @Override
     public void add(Task task) {
-        if (historyList.size() == MAX_HISTORY_SIZE) {
+        if (historyList.size() == maxHistorySize) {
             historyList.removeFirst();
         }
         historyList.add(task);
