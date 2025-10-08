547fce92f2812d1290815970a6da4f4073eb52e5
Leonid Proskuryakov <lpr@inbox.ru>
2025-10-08 13:05:17 +0300

    Исправление: '{' is not preceded with whitespace.

--- src/practicum/taskmanager/InMemoryHistoryManager.java
+++ src/practicum/taskmanager/InMemoryHistoryManager.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class InMemoryHistoryManager implements HistoryManager{
+public class InMemoryHistoryManager implements HistoryManager {
     private Node first;
     private Node last;
     private Map<Integer, Node> idToNode;
